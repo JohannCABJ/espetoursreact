df4a4610cf2218b6287161db1400a43554554c47
Johann Triana <johann.rvs@gmail.com>
2023-12-21 17:52:16 -0500

    Refactor code to improve performance and readability

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+import productions from './builders/productions';
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     host: '192.168.1.73', // Cambia '0.0.0.0' a tu dirección IP local si deseas exponer la aplicación en la red local
     port: 5173, // Cambia el puerto a tu puerto deseado
+  },
+
+  build: {
+    rollupOptions: {
+      plugins: [productions()],
+    },
   },
 })
